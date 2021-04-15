7bf6c01e1af6b3b57001fece0bafc4e1598ea5e5
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-15 23:46:59 +0530

    ADDED : Compute Factorial Number

---
+++ ForLoopPrograms/factorialNumber.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+
+echo -n "Enter a number"
+read number
+
+#variables
+fact=1
+
+for (( i=2; i<=$number; i++ ))
+do
+	fact=$(( $fact * $i ))
+done
+
+echo "Factorial of $number is $fact"

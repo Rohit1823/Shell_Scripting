f037c7b4408caf43c9544414ef17c80a779beef6
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-15 22:56:56 +0530

    ADDED : Write a program to generate a birth month

---
+++ Dictionary/birthMonth.sh
@@ -0,0 +1,15 @@
+#! /bin/bash -x
+
+TOTAL_PERSONS=50
+MONTHS=12
+
+for (( person=1; person<=$TOTAL_PERSONS; person++ ))
+do
+    birthMonth=$(( RANDOM % MONTHS + 1 ))
+    birthdays[birthMonth]="${birthdays[birthMonth]} $person,"
+done
+
+for (( month=1; month<=$MONTHS; month++ ))
+do
+    echo "People who have Birthday in month $month: ${birthdays[month]}"
+done

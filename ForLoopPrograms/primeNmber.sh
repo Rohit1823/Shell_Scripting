f964a9220d9578a678c318b9600d13a749f3d828
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-15 23:43:10 +0530

    ADDED : Determines if the number is prime

---
+++ ForLoopPrograms/primeNmber.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+echo -n "Enter a number: "
+read number
+
+for (( i=2; i<$number; i++ ))
+do
+   if [[ $(( $number % $i )) -eq 0 ]]
+   then
+       echo "not a prime number"
+       break
+   fi
+done
+
+if [[ $i -eq $number ]]
+then
+   echo "prime number"
+fi

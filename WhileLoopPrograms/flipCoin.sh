3ce4e9107e1224f9ab4dc00cc2dfcfa7f1b2900c
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-15 23:57:58 +0530

    ADDED : Flip Coin Problem

---
+++ WhileLoopPrograms/flipCoin.sh
@@ -0,0 +1,32 @@
+#! /bin/bash -x
+
+#Problem Statement : Flip coin problem
+
+#Date : 6 April 2021
+
+#Name : Rohit Machale
+
+isTail=0
+isHead=1
+maxWin=11
+tailCount=0
+headCount=0
+randomFlip=0
+
+while [[ $tailCount -lt $maxWin && $headCount -lt $maxWin ]]
+do
+   randomFlip=$(( RANDOM % 2 ))
+case $randomFlip in
+$isTail)
+(( taiCount++ ));;
+$isHead)
+(( headCount++ ));;
+esac
+done
+
+if [ $tailCount -gt $headCount ]
+then
+    echo "Tail wins $tailCount and Head count is $headCount"
+else
+    echo "Head wins $headCount and Tail count is $tailCount"
+fi
